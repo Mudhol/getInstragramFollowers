45cc451c296457d6c120004911472c72b9afc1a4
Siddharamesh <46159173+Mudhol@users.noreply.github.com>
2020-06-06 21:57:56 +0530

    Add files via upload
    The main file to be uploaded

---
+++ main.py
@@ -0,0 +1,74 @@
+#You need to install python3 before using this code
+#Install selenium and time using pip
+#Download the chromium webdriver and add it to your PATH
+#For more information on how to use selenium, visit https://selenium-python.readthedocs.io/installation.html
+
+
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class InstaBot:
+    def __init__(self, username, pw):
+        self.driver = webdriver.Chrome()
+        self.username = username
+        self.driver.get("https://instagram.com")
+        # sleep(2)
+        # self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]")\
+        #     .click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+            .send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
+            .send_keys(pw)
+        self.driver.find_element_by_xpath('//button[@type="submit"]')\
+            .click()
+        sleep(4)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
+        sleep(2)
+
+    def get_unfollowers(self):
+        self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username))\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//a[contains(@href,'/following')]")\
+            .click()
+        following = self._get_names()
+        self.driver.find_element_by_xpath("//a[contains(@href,'/followers')]")\
+            .click()
+        followers = self._get_names()
+        not_following_back = [
+            user for user in following if user not in followers]
+        print(not_following_back)
+        self.driver.close()
+
+    def _get_names(self):
+        sugs = self.driver.find_element_by_xpath(
+            '//h4[contains(text(), Suggestions)]')
+        self.driver.execute_script('arguments[0].scrollIntoView()', sugs)
+        sleep(2)
+        scroll_box = self.driver.find_element_by_xpath(
+            "/html/body/div[4]/div/div[2]")
+        last_ht, ht = 0, 1
+        while last_ht != ht:
+            last_ht = ht
+            sleep(1)
+            ht = self.driver.execute_script("""
+                arguments[0].scrollTo(0, arguments[0].scrollHeight); 
+                return arguments[0].scrollHeight;
+                """, scroll_box)
+        links = scroll_box.find_elements_by_tag_name('a')
+        names = [name.text for name in links if name.text != '']
+        # close button
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/div[1]/div/div[2]/button")\
+            .click()
+        return names
+
+#Enter your username and password in the dialogue below
+my_bot = InstaBot(username, password)
+my_bot.get_unfollowers()
